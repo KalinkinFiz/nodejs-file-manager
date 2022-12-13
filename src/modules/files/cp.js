fd447c6928dae1d87f9ebc8cb7dc241c8074691f
KalinkinFiz <kalinkin.fiz@gmail.com>
2022-12-13 16:51:47 +0300

    feat: modules cp done

---
+++ src/modules/files/cp.js
@@ -0,0 +1,21 @@
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream";
+import { promisify } from "util";
+import path from "path";
+
+const pipelineAsync = promisify(pipeline);
+
+export const copy = async (pathToFile, pathToNewDirectory) => {
+  try {
+    const fileName = pathToFile.slice(pathToFile.lastIndexOf(path.sep) + 1);
+
+    await pipelineAsync(
+      createReadStream(pathToFile),
+      createWriteStream(path.join(pathToNewDirectory, fileName))
+    );
+  } catch {
+    throw new Error("Operation failed");
+  }
+};
+
+await copy("./ad.txt", "../");
