a36d025b0e249b16271f006b5b3e44515f53334f
KalinkinFiz <kalinkin.fiz@gmail.com>
2022-12-19 23:47:55 +0300

    feat: add rl files

---
+++ src/index.js
@@ -0,0 +1,111 @@
+import { createInterface } from "readline/promises";
+import { homedir } from "os";
+
+import * as modules from "./modules/index.js";
+
+const args = process.argv[2];
+
+export const readline = async (args) => {
+  if (!args) {
+    console.log("Please enter user name (--username=your_name)");
+    return;
+  }
+
+  const param = args.slice(2).split("=");
+  const [key, username] = param;
+
+  if (key === "username") {
+    console.log(`Welcome to the File Manager, ${username}!`);
+    process.chdir(homedir());
+    console.log(`You are currently in ${process.cwd()}`);
+  } else {
+    console.log("Please enter user name (--username=your_name)");
+    return;
+  }
+
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.on("line", async (line) => {
+    const [command, argument, secArgument] = line.includes('"')
+      ? line
+          .split('"')
+          .map((item) => item.trim())
+          .filter((item) => item)
+      : line.split(" ");
+    try {
+      switch (command) {
+        case ".exit": {
+          rl.close();
+          break;
+        }
+        case "cat": {
+          await modules.cat(argument);
+          break;
+        }
+        case "add": {
+          await modules.add(argument);
+          break;
+        }
+        case "rn": {
+          await modules.ren(argument, secArgument);
+          break;
+        }
+        case "cp": {
+          await modules.copy(argument, secArgument);
+          break;
+        }
+        case "mv": {
+          await modules.mv(argument, secArgument);
+          break;
+        }
+        case "rm": {
+          await modules.remove(argument);
+          break;
+        }
+        case "up": {
+          await modules.up();
+          break;
+        }
+        case "ls": {
+          await modules.ls();
+          break;
+        }
+        case "cd": {
+          await modules.cd(argument);
+          break;
+        }
+        case "os": {
+          await modules.castomOS(argument);
+          break;
+        }
+        case "hash": {
+          await modules.calcHash(argument);
+          break;
+        }
+        case "compress": {
+          await modules.compress(argument, secArgument);
+          break;
+        }
+        case "decompress": {
+          await modules.decompress(argument, secArgument);
+          break;
+        }
+        default: {
+          console.log("Invalid input");
+        }
+      }
+      console.log(`You are currently in ${process.cwd()}`);
+    } catch (err) {
+      console.log(err.message);
+    }
+  });
+
+  rl.on("close", () =>
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`)
+  );
+};
+
+await readline(args);
